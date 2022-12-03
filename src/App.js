8fc2b5f8d1621fc04577ac67405631d2d6f4ed45
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2022-12-03 19:20:40 +0900

    2022-12-03

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Mainpage from './pages/MainPage';
 import SidePage from './pages/SidePage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/side" element={<SidePage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return <h1>디테일 페이지</h1>;
+};
+export default AboutPage;
